cd605a5136b0f36835ca42d301b9f0ca5d2562e7
Daniel Gray <daniel@tangentsolutions.co.za>
2024-03-20 13:11:00 +0200

    Add Project.subjects

--- app/general/models.py
+++ app/general/models.py
@@ -10,6 +10,7 @@ class Project(models.Model):
     Institution = models.ForeignKey(
         "Institution", on_delete=models.CASCADE, blank=True, verbose_name=("institution")
     )
+    subjects = models.ManyToManyField("Subject", blank=True)
 
     def __str__(self):
         return self.name
