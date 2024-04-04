da36766cd237605a3975324267d71c3515cab2ba
OMosimege <onalerona.mosimege@gmail.com>
2024-04-04 14:51:12 +0200

    Frontend styling with basic front page

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
 ]
