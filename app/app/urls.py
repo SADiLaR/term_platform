eb5af8665dec9d34991cf458801455dd0f7fe98e
Daniel Gray <daniel@tangentsolutions.co.za>
2024-04-15 15:22:57 +0200

    Added document file upload to models
    - updated Admin forms with validation
    - added tests

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -34,3 +36,6 @@ handler400 = "app.views.error_400"
 handler403 = "app.views.error_403"
 handler404 = "app.views.error_404"
 handler500 = "app.views.error_500"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
