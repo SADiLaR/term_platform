6ffcb26c27de4e09fd87dd8e1941f3e52b44c3d8
“OMosimege” <onalerona.mosimege@gmail.com>
2024-05-23 08:43:02 +0200

    Add calculation for profile completion
    Add new efficient way to calculate completion weighting, add to template and unit test

--- app/app/views.py
+++ app/app/views.py
@@ -24,9 +24,34 @@ def institutions(request):
     context = {}
 
     institutions = Institution.objects.annotate(project_count=Count("project")).order_by("name")
+
+    institutions_array = []
+
+    for institution in institutions:
+        institution_dict = {
+            "name": institution.name,
+            "abbreviation": institution.abbreviation,
+            "url": institution.url,
+            "email": institution.email,
+            "logo": institution.logo,
+        }
+        completed_fields_count = sum(1 for value in institution_dict.values() if value)
+
+        """Rating returns % number of completed fields.
+
+        Profile completion weighting is % number of completed fields in 5 star rating.
+
+        Profile completion is calculated using the number of present model fields,
+        model fields have to be added to the institution_dict"""
+
+        rating = (completed_fields_count / len(institution_dict)) * 100
+        institution_dict["rating"] = round(rating)
+        institution_dict["project_count"] = institution.project_count
+        institutions_array.append(institution_dict)
+
     context = {
         "current_page": "institutions",
-        "institutions": institutions,
+        "institutions": institutions_array,
     }
 
     return render(request, template_name=template, context=context)
