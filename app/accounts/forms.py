e9a0bc2aca9182ff99245704273bbf9fcf86f2f5
“OMosimege” <onalerona.mosimege@gmail.com>
2024-07-10 14:26:12 +0200

    Add user manangement views, templates, and styling

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,14 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Required. Add a valid email address.")
-    username = forms.CharField(required=True, help_text="Required. Add a valid username.")
-    first_name = forms.CharField(required=True, help_text="Required. Add a valid first name.")
-    last_name = forms.CharField(required=True, help_text="Required. Add a valid last name.")
+    email = forms.EmailField(
+        required=True,
+        help_text=_("Required. Add a valid email address."),
+    )
+    username = forms.CharField(
+        required=True,
+        help_text=_("Required. Add a valid username."),
+    )
+    first_name = forms.CharField(
+        required=True,
+        help_text=_("Required. Add a valid first name."),
+    )
+    last_name = forms.CharField(
+        required=True,
+    )
 
     class Meta:
         model = CustomUser
@@ -16,7 +28,15 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields["email"].required = True
-        self.fields["username"].required = True
-        self.fields["first_name"].required = True
-        self.fields["last_name"].required = True
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label=_("Username"), help_text=_("Required. Enter your username."))
+    password = forms.CharField(label=_("Password"), help_text=_("Required. Enter your password."))
+
+    def __init__(self, *args, **kwargs):
+        super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
