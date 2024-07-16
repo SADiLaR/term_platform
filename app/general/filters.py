6f558f6762ab0b01935f66a5f444d33e6e66af38
Daniel Gray <daniel@tangentsolutions.co.za>
2024-07-16 08:27:01 +0200

    added projects to search filters
    - Combine search queries with union
    - updated tests

--- app/general/filters.py
+++ app/general/filters.py
@@ -1,10 +1,16 @@
 import django_filters
 from django import forms
-from django.contrib.postgres.search import SearchHeadline, SearchQuery, SearchRank
-from django.db.models import F
+from django.contrib.postgres.search import (
+    SearchHeadline,
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+)
+from django.db.models import F, Value
+from django.db.models.functions import Greatest, Left
 from django_filters import ModelMultipleChoiceFilter, MultipleChoiceFilter
 
-from general.models import DocumentFile, Institution, Language, Subject
+from general.models import DocumentFile, Institution, Language, Project, Subject
 
 
 class DocumentFileFilter(django_filters.FilterSet):
@@ -33,27 +39,61 @@ class DocumentFileFilter(django_filters.FilterSet):
         ]
 
     def filter_queryset(self, queryset):
+        # More information about weighting and normalization in postgres:
+        # https://www.postgresql.org/docs/current/textsearch-controls.html#TEXTSEARCH-RANKING
         queryset = super().filter_queryset(queryset)
 
-        search = self.form.cleaned_data.get("search", "")
-        queue = SearchQuery(search.strip())
-        search_rank = SearchRank(F("search_vector"), queue)
-        search_headline = SearchHeadline("document_data", queue)
-        queryset = (
-            queryset.annotate(
-                rank=search_rank,
-                search_headline=search_headline,
-            )
-            .defer("document_data")
-            .select_related("institution")
-        ).order_by("-rank")
+        search = self.form.cleaned_data.get("search", "").strip()
+        query = SearchQuery(search)
 
-        return queryset
+        # A fixed list of identical fields are required to join queries of
+        # different classes with `.union()`:
+        fields = (
+            "id",
+            "heading",
+            "description",
+            "rank",
+            "search_headline",
+            "view",
+            "logo_url",
+            "logo_url",
+        )
+
+        # In the queries below, any differences between models must be fixed
+        # through e.g. `Value` or `F` annotations.
+        project_search_vector = SearchVector("name", weight="A") + SearchVector(
+            "description", weight="B"
+        )
+        project_query = (
+            Project.objects.annotate(
+                heading=F("name"),
+                view=Value("project_detail"),
+                logo_url=F("logo"),
+                search_headline=SearchHeadline("description", query),
+                rank=SearchRank(project_search_vector, query, normalization=16),
+                search=project_search_vector,
+            )
+            .filter(search=query)
+            .values(*fields)
+        )
+
+        # We limit the headline to limit the performance impact. On very large
+        # documents, this slows things down if unconstrained.
+        search_headline = SearchHeadline(Left("document_data", 200_000), query)
+        search_rank = SearchRank(F("search_vector"), query, normalization=16)
+        queryset = queryset.annotate(
+            heading=F("title"),
+            view=Value("document_detail"),
+            logo_url=Value("no_logo"),
+            rank=search_rank,
+            search_headline=search_headline,
+        ).values(*fields)
+        return queryset.union(project_query).order_by("-rank")
 
     def filter_search(self, queryset, name, value):
         if value:
-            queue = SearchQuery(value.strip())
-            return queryset.filter(search_vector=queue)
+            query = SearchQuery(value.strip())
+            return queryset.filter(search_vector=query)
 
         else:
             return queryset
