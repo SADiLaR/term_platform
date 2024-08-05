db7e9f5f68222d8058e8d036a15eb79ba7cc645a
Friedel Wolff <friedel.wolff@nwu.ac.za>
2024-08-05 14:26:58 +0200

    A partial base template for htmx

--- app/general/context_processors.py
+++ app/general/context_processors.py
@@ -4,4 +4,5 @@ from django.conf import settings
 def template_vars(request):
     return {
         "debug_toolbar": settings.DEBUG and settings.DEBUG_TOOLBAR,
+        "BASE_TEMPLATE": "base_htmx.html" if request.htmx else "base.html",
     }
