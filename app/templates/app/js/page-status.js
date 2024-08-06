64cda4b794b0016716ff99e5dadcbbb10c67dd3e
Friedel Wolff <friedel.wolff@nwu.ac.za>
2024-08-06 21:47:13 +0200

    Rework HTMX error handling
    This uses a single error block for all errors for uniform appearance.
    It moves the block to the base template, with out-of-band swaps for
    the htmx base template.
    
    This tries to handle a lot of corner cases, like JS disabled, htmx
    not loading, etc.

--- app/templates/app/js/page-status.js
+++ app/templates/app/js/page-status.js
@@ -1,29 +1,38 @@
+/*{% load i18n %}
 {% comment %}
 NOTE: this is a Django template that renders a JavaScript file that is inlined
 in the base template. This way it can easily be edited as a JS file, and keeps
 the base template simple.
 
 Django template comments are used to reduce the payload.
-{% endcomment %}
-const errorTag = document.getElementById("error");
-const errorText = document.getElementById("error-text");
-{# Some response, so can just render page normally. Hide error. #}
-function handleSuccess(evt) {
-    errorTag.setAttribute("hidden", "true")
-    errorText.innerText = "";
+
+This requires care to test well. Scenarios:
+ * Fresh load of pages 200 / 404.
+ * From freshly loaded 200 click on 404.
+ * From freshly loaded 404 click on 200.
+ * From a 200 page, click on anything while server is down.
+ * From a 404 page, click on anything while server is down.
+In each case the error box should appear/disappear as necessary.
+
+Furthermore:
+ - Test with JS disabled.
+ - Test with HTMX not (yet) loaded.
+
+{% endcomment %}*/
+function handleAfterRequest(evt) {
+    const errorBlock = document.getElementById("error-block");
+    if (evt.detail.successful) {
+        errorBlock.setAttribute("hidden", "true")
+    } else {
+	if (typeof evt.detail.failed === "undefined") {
+            //{# Not an error page. Probably network problems. #}
+            const errorTitle = document.getElementById("error-title");
+            errorTitle.innerText = "{% trans 'Network error' %}";
+            const errorMessage = document.getElementById("error-message");
+            errorMessage.innerText = "{% trans 'Couldn’t load the page. Check your network connection and try to refresh the page.' %}";
+        }
+        errorBlock.removeAttribute("hidden");
+        errorBlock.scrollIntoView();
+   }
 }
-{# Not an HTTP error like 500. Maybe network error. #}
-function handleError(evt) {
-    errorTag.removeAttribute("hidden");
-    errorText.innerText = "Couldn’t load the page. Check your network connection and try to refresh the page.";
-    errorTag.scrollIntoView();
-    {# If the currently showing page is an error page (e.g. 404), hide it. #}
-    errorPage = document.getElementById("error-page");
-    if (errorPage) {
-        errorPage.setAttribute("hidden", "true");
-    }
-}
-htmx.on("htmx:afterRequest", handleSuccess);
-htmx.on("htmx:responseError", handleSuccess);{# Got an error page, hide #error #}
-htmx.on("htmx:sendError", handleError);
-htmx.on("htmx:timeout", handleError);
+if (typeof htmx !== "undefined") htmx.on("htmx:afterRequest", handleAfterRequest);
