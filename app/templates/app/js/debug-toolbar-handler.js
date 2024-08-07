9d67eea843180023e4f8e65adc9441e05a427287
Friedel Wolff <friedel.wolff@nwu.ac.za>
2024-08-07 16:41:17 +0200

    Hide Django template comments with real comments
    This makes these at least these files valid CSS/JS even when editing.

--- app/templates/app/js/debug-toolbar-handler.js
+++ app/templates/app/js/debug-toolbar-handler.js
@@ -1,11 +1,11 @@
-{% comment %}
+/*{% comment %}
 NOTE: this is a Django template that renders a JavaScript file that is inlined
 in the base template. This way it can easily be edited as a JS file, and keeps
 the base template simple.
 
 Since we use HTMX, add the event handler as recommended here:
 https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#htmx
-{% endcomment %}
+{% endcomment %}*/
 if (typeof window.htmx !== "undefined") {
     htmx.on("htmx:afterSettle", function(detail) {
         if (
