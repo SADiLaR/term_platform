3a266a2a766ed19eae95a4cd032540b03ead3174
Friedel Wolff <friedel.wolff@nwu.ac.za>
2024-08-10 20:50:25 +0200

    Make loader more accessible

--- app/templates/app/js/page-status.js
+++ app/templates/app/js/page-status.js
@@ -18,13 +18,22 @@ Furthermore:
  - Test with JS disabled.
  - Test with HTMX not (yet) loaded.
 
+ If an HTMX event takes too long, #loader gives a visual indication and we set
+ the text for accessibility purposes. This will be quite chatty if done on
+ every request, so we only add the text if it takes longer than a timeout. If
+ things are working well, the new content should be announced (and announced in
+ good time), so the loader is only needed to confirm that the request is
+ submitted if things are taking long enough that the user might have doubts.
 {% endcomment %}*/
 function handleAfterRequest(evt) {
     const errorBlock = document.getElementById("error-block");
+    const loaderText = document.getElementById("loader-text");
     if (evt.detail.successful) {
+        clearTimeout(timeoutID);
+        loaderText.innerText = "";
         errorBlock.setAttribute("hidden", "true")
     } else {
-	if (typeof evt.detail.failed === "undefined") {
+        if (typeof evt.detail.failed === "undefined") {
             //{# Not an error page. Probably network problems. #}
             const errorTitle = document.getElementById("error-title");
             errorTitle.innerText = "{% trans 'Network error' %}";
@@ -35,4 +44,15 @@ function handleAfterRequest(evt) {
         errorBlock.scrollIntoView();
    }
 }
-if (typeof htmx !== "undefined") htmx.on("htmx:afterRequest", handleAfterRequest);
+
+function handleBeforeRequest(evt) {
+    timeoutID = setTimeout(function () {
+        const loaderText = document.getElementById("loader-text");
+        loaderText.innerText = "{% trans 'Loading...' %}";
+    }, 1000)
+}
+
+if (typeof htmx !== "undefined") {
+    htmx.on("htmx:beforeRequest", handleBeforeRequest);
+    htmx.on("htmx:afterRequest", handleAfterRequest);
+}
