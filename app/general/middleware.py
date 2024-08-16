0118920733a8083dd2688c72c599b7223833a85e
Friedel Wolff <friedel.wolff@nwu.ac.za>
2024-08-16 22:15:04 +0200

    Ensure HTML pages vary on HX-Request
    This fixes the problem with a partial page showing on browser restore,
    or for example with Firefox plugin "Auto Tab Discard" if a tab is
    restored. It will also be required if "real" caching is implemented.

---
+++ app/general/middleware.py
@@ -0,0 +1,17 @@
+from django.utils.cache import patch_vary_headers
+
+
+class ExtraVaryMiddleware:
+    """Ensure HTML pages vary on HX-Request
+
+    This is needed so that incomplete responses based on base_htmx.html are not
+    reused as full-page responses, for example on browser restore of a page."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if "text/html" in response.headers["Content-Type"]:
+            patch_vary_headers(response, ["HX-Request"])
+        return response
