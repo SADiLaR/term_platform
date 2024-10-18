7fa75fb58f874fa90cfb7648178df88f77f6acd0
Restioson <restiosondev@gmail.com>
2024-10-18 16:07:27 +0200

    fixup! chore(ci): set up Lighthouse auditing in CI

---
+++ app/lighthouserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    ci: {
+        collect: {
+            startServerCommand: "python manage.py runserver localhost:3000",
+                url: [
+                    "http://localhost:3000",
+                    "http://localhost:3000/search/",
+                    "http://localhost:3000/institutions/",
+                    "http://localhost:3000/projects/",
+                    "http://localhost:3000/documents/",
+                    "http://localhost:3000/languages/",
+                    "http://localhost:3000/subjects/"
+                ]
+        },
+        assert: {
+            "preset": "lighthouse:recommended",
+            "assertions": {
+                // We are running this against a Django testing server
+                "is-on-https": false,
+            }
+        }
+    }
+}
