ce0bdf4fe7f620cc9e5e8d9f12b1d59a89372864
Restioson <restiosondev@gmail.com>
2024-11-06 13:47:21 +0200

    test(selenium): run tests across multiple {browser, js} combinations
    By passing the browser type and whether JS is enabled via environment variables,
    we can run the Selenium frontend tests on all combinations of targetted
    browsers and JS/no-JS in the CI pipeline. For now, we are not targetting Safari
    for this, as the GitHub Actions MacOS workers don't suppose `services`, which
    we use to run the postgres database. This will be left for a follow-up (see
    issue #145).

--- Makefile
+++ Makefile
@@ -75,7 +75,7 @@ create-schema:
 	@docker compose run --rm web python manage.py graph_models -a -o schema/schema.png
 
 test:
-	@docker compose run --rm web python manage.py test $(module)
+	@docker compose run --rm -e BROWSER -e JS_ENABLED  web python manage.py test $(module)
 
 ruff-check:
 	@docker compose run --rm web ruff check .
--- app/general/tests/test_frontend.py
+++ app/general/tests/test_frontend.py
@@ -1,8 +1,21 @@
+import os
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import tag
 from selenium.common import TimeoutException
-from selenium.webdriver.chrome.webdriver import Options, WebDriver
+from selenium.webdriver.chrome.webdriver import (
+    Options as ChromeOptions,
+)
+from selenium.webdriver.chrome.webdriver import (
+    WebDriver as ChromeWebDriver,
+)
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import (
+    Options as FirefoxOptions,
+)
+from selenium.webdriver.firefox.webdriver import (
+    WebDriver as FirefoxWebDriver,
+)
 from selenium.webdriver.support.wait import WebDriverWait
 
 # Wait timeout in seconds
@@ -13,19 +26,53 @@ WAIT_TIMEOUT = 5
 class TestFrontend(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
-        opts = Options()
-        opts.add_argument("--headless")
-        opts.add_argument("--no-sandbox")
-        opts.add_argument("--disable-dev-shm-usage")
-        opts.add_argument("--window-size=1920,1080")
-        cls.driver = WebDriver(opts)
+        browser = os.environ.get("BROWSER", "chrome").lower()
+        cls.js_enabled = os.environ.get("JS_ENABLED", "js-enabled") == "js-enabled"
+
+        print(
+            f"Running Selenium tests on {browser}. JS is {'enabled' if cls.js_enabled else 'disabled'}."
+        )
+
+        if browser == "chrome":
+            opts = ChromeOptions()
+            opts.add_argument("--headless=new")
+            opts.add_argument("--no-sandbox")
+            opts.add_argument("--disable-dev-shm-usage")
+            opts.add_argument("--window-size=1920,1080")
+
+            if not cls.js_enabled:
+                # Taken from https://stackoverflow.com/a/73961818
+                prefs = dict()
+                prefs["webkit.webprefs.javascript_enabled"] = False
+                prefs["profile.content_settings.exceptions.javascript.*.setting"] = 2
+                prefs["profile.default_content_setting_values.javascript"] = 2
+                prefs["profile.managed_default_content_settings.javascript"] = 2
+                opts.add_experimental_option("prefs", prefs)
+                opts.add_argument("--disable-javascript")
+
+            cls.driver = ChromeWebDriver(opts)
+        elif browser == "firefox":
+            options = FirefoxOptions()
+            options.add_argument("-headless")
+            options.set_preference("javascript.enabled", cls.js_enabled)
+            cls.driver = FirefoxWebDriver(options=options)
+
         cls.driver.implicitly_wait(WAIT_TIMEOUT)
+
         super().setUpClass()
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
         super().tearDownClass()
+
+    # Checks that JS is properly disabled if passed through env var, else check if enabled
+    def test_js_enabled_or_disabled(self):
+        test_dir = os.getenv("TESTING_DIR", "/app/general/tests/files")
+        self.driver.get(f"file://{test_dir}/test_js.html")
+        self.assertEqual(
+            len(self.driver.find_elements(By.ID, "js-enabled")), 1 if self.js_enabled else 0
+        )
 
     def test_no_404s(self):
         # Sanity check in case we ever change the 404 title
